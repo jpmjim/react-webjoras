7b60077f09eb6a2920ffc49e00754b43fe9c3419
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-05-02 20:20:17 -0500

    seccion de navegacion

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='header__name'>JORAS</h1>
+      <nav className='header__nav'>
+        <ul>
+          <li><a href=""></a>Inicio</li>
+          <li><a href=""></a>Nosotros</li>
+          <li><a href=""></a>Servicios</li>
+          <li><a href=""></a>Portafolio</li>
+          <li><a href=""></a>Blog</li>
+          <li><a href=""></a>Contacto</li>
+        </ul>
+      </nav>
+      <section className='header__search'>
+        <input type="" placeholder="Buscar" name="" id="" /><span class="bi bi-search logo__search"></span>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
