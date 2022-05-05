adab8c9af6e21d5550b8dcbe8425efc3c6fd4673
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-05-04 20:38:58 -0500

    footer responsive

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer__redes'>
+        <h2>Nuestras redes:</h2>
+        <nav className='footer__nav'>
+          <a className='bi bi-facebook' href=""></a>
+          <a className='bi bi-youtube' href=""></a>
+          <a className='bi bi-twitter' href=""></a>
+          <a className='bi bi-linkedin' href=""></a>
+        </nav>
+      </div>
+      <div className='footer__copy'>
+        <p>Copyright © 2021, JORAS | All Right Reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
