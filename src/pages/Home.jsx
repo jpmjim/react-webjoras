fac2a8af7b80f031393b563f9ced87a02172e08b
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-05-04 20:14:56 -0500

    titulo y description de la pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,12 @@ const Home = () => {
   return (
     <main>
       <section className='inicio'>
-
+        <div className='inicio__title'>
+          <h2>JORAS</h2>
+        </div>
+        <div className='inicio__description'>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem vel recusandae placeat accusantium reiciendis minus aperiam laborum iste quisquam iusto blanditiis, nemo aspernatur incidunt, temporibus voluptas, natus architecto est ut!</p>
+        </div>
       </section>
     </main>
   );
