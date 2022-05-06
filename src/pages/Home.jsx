76a76c62cdf095ca0ab9001e767700d527783138
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-05-05 19:03:40 -0500

    section servicios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from '../components/About';
+import Service from '../components/Service';
 
 const Home = () => {
   return (
@@ -9,10 +10,11 @@ const Home = () => {
           <h2>JORAS</h2>
         </div>
         <div className='inicio__description'>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem vel recusandae placeat accusantium reiciendis minus aperiam laborum iste quisquam iusto blanditiis, nemo aspernatur incidunt, temporibus voluptas, natus architecto est ut!</p>
+          <p>" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem vel recusandae placeat accusantium reiciendis minus aperiam laborum iste quisquam iusto blanditiis, nemo aspernatur incidunt, temporibus voluptas, natus architecto est ut! "</p>
         </div>
       </section>
       <About />
+      <Service />
     </main>
   );
 }
