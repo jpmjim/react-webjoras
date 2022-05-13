fe03464e3f4ee45a755c74534baacb28b060bf56
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-05-12 21:10:58 -0500

    card de nosotros

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import user from '../assets/img/not-found.png';
 
 const About = () => {
   return (
     <section className='about'>
       <h2>Sobre Nosotros</h2>
+      <section className='about__miembro'>
+        <img src={user} alt="" />
+        <p>Jimmy Joel Pecho Malqui</p>
+      </section>
     </section>
   );
 }
