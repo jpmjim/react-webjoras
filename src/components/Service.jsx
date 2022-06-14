c1308740e7bbdff17db45dbe66288d4f06753875
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-06-13 20:57:35 -0500

    inicio con los servicios

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,6 +4,22 @@ const Service = () => {
   return (
     <section className='service'>
       <h2>Servicios</h2>
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th><i className='bi bi-boostrap'></i> Frontend</th>
+              <th>Backend</th>
+              <th>BaseData</th>
+            </tr>
+            <tr>
+              <th>Backend</th>
+              <th>Backend</th>
+              <th>Backend</th>
+            </tr>
+          </thead>
+        </table>
+      </section>
     </section>
   );
 }
