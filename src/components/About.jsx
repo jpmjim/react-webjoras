de1374ca07e76657980b1ea9ac8e7c60d96a2968
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-06-13 20:53:09 -0500

    cambios en about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
     <section className='about'>
       <h2>Sobre Nosotros</h2>
       <section className='miembros'>
+        <p className='miembros__description'>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nemo quaerat recusandae aut ipsa dolorum esse adipisci excepturi officiis quod aliquid reiciendis magni quo, consequuntur iusto eius hic laudantium corporis"</p>
         <section className='about__miembro'>
           <img src={user} alt="" />
           <p>Jimmy Joel Pecho Malqui</p>
